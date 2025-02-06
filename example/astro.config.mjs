8fb3c0c3cf1ef047b20da595948957f6686a61d1
Jan-Joost den Brinker <janjoost.denbrinker@agonos.io>
2025-02-06 12:10:18 +0100

    using updated way for env variables

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -2,7 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tolgee from '../src/index';
 import { DevTools, FormatSimple } from '@tolgee/web';
-
+import { loadEnv } from "vite";
+const { TOLGEE_API_KEY } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 // For production, passing in static files
 import localeEn from "./public/i18n/en.json";
 import localeFr from "./public/i18n/fr.json";
@@ -15,10 +16,7 @@ export default defineConfig({
             options: {
                 language: 'en',
                 apiUrl: 'https://app.tolgee.io',
-                apiKey: process.env.TOLGEE_API_KEY,
-                observerOptions: {
-                    fullKeyEncode: true,
-                },
+                apiKey: TOLGEE_API_KEY,
                 // For production
                 staticData: {
                     'en': localeEn,
